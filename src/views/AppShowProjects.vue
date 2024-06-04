733bbeb3b6c281798e45653443cb9dbc716978b8
GioMench <giorgiaglm@gmail.com>
2024-06-04 17:15:44 +0200

    error in AppShowprojects

--- src/views/AppShowProjects.vue
+++ src/views/AppShowProjects.vue
@@ -11,15 +11,41 @@ export default {
             loading: true
         }
     },
-    mounted(){
+    mounted() {
         console.log(this.$route.params.slug, this.base_api_url, this.base_projects_url)
+        const url = this.base_api_url + this.base_projects_url + `/${this.$route.params.slug}`
+        //console.log(url)
+
         
+     axios.get(url)
+     .then(response => {
+       console.log(response)
+      if(response.data.success) {
+        // get the data
+        console.log(response.data.response)
+        this.project = response.data.response
+        this.loading = false
+      }else{
+        //404
+      }})
+
     }
 }
 </script>
 
-<template>
-  project
+<template v-if="project">
+
+
+    <div class="card">
+
+        <img :src="base_api_url + 'storage/' + project.preview_image" alt="">
+
+        <div class="card-body">
+            <h3>{{ project.project_name }}</h3>
+        </div>
+
+    </div>
+
 </template>
 
 <style></style>
